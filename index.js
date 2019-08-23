9672b47dc1ae78d31f3c14c38c581345661eeff9
Ilmast Toivo <s1900624@edu.bc.fi>
2019-08-23 12:39:31 +0300

    Add: detect pressing ´Enter´ key and other keys

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 window.onload = function() {
-
+    document.addEventListener('keydown', function(event) {
+        event.code === 'Enter' ? alert('Bravo!') : alert('Sorry, try again ..');
+    });
 };
