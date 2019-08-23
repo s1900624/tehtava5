d5594b81d0843c6a6918d780ba4d4d5044b1fc78
Ilmast Toivo <s1900624@edu.bc.fi>
2019-08-23 12:46:07 +0300

    Update: second ´alert()´ was changed to ´console.log()´

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload = function() {
     document.addEventListener('keydown', function(event) {
-        event.code === 'Enter' ? alert('Bravo!') : alert('Sorry, try again ..');
+        event.code === 'Enter' ? alert('Bravo!') : console.log('Sorry, try again ..');
     });
 };
